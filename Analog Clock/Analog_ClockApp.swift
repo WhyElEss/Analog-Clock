4ec5addfe5b68489cf2f7544d79bdf784059d79e
Yuri Stanislavsky <stanislavskiy@me.com>
2020-09-18 20:16:16 +0300

    Initial Commit

---
+++ Analog Clock/Analog_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Analog_ClockApp.swift
+//  Analog Clock
+//
+//  Created by Юрий Станиславский on 18.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct Analog_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
